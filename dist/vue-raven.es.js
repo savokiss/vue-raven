2b5076fb3c34c2d49d4c5e76e7c82d441c58a0bb
savokiss <jaynaruto@qq.com>
2018-07-17 17:37:52 +0800

    chore: globals

--- dist/vue-raven.es.js
+++ dist/vue-raven.es.js
@@ -1,5 +1,5 @@
 /*!
- * @ecg/vue-raven v2.0.0
+ * @ecg/vue-raven v2.0.2
  * (c) 2018-present savokiss <jaynaruto@qq.com>
  * Released under the MIT License.
  */
@@ -7,11 +7,29 @@ import Raven from 'raven-js';
 export { default as Raven } from 'raven-js';
 import RavenVue from 'raven-js/plugins/vue';
 
+function _extends() {
+  _extends = Object.assign || function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+
+  return _extends.apply(this, arguments);
+}
+
 var plugin = {
   install: function install(Vue) {
     var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-    var _options = Object.assign({
+    var _options = _extends({
       dsn: options.dsn || '',
       version: options.version || '',
       disableReport: options.disableReport || false,
