37cc6995d99db04b3fd083a543f0a5f2804b5963
savokiss <jaynaruto@qq.com>
2018-09-06 13:28:53 +0800

    release: v2.2.0

--- dist/vue-raven.es.js
+++ dist/vue-raven.es.js
@@ -1,11 +1,26 @@
 /*!
- * @ecg/vue-raven v2.1.0
+ * @ecg/vue-raven v2.2.0
  * (c) 2018-present savokiss <jaynaruto@qq.com>
  * Released under the MIT License.
  */
 import Raven from 'raven-js';
 export { default as Raven } from 'raven-js';
 import RavenVue from 'raven-js/plugins/vue';
+
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+}
 
 function _extends() {
   _extends = Object.assign || function (target) {
@@ -25,6 +40,25 @@ function _extends() {
   return _extends.apply(this, arguments);
 }
 
+function _objectSpread(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i] != null ? arguments[i] : {};
+    var ownKeys = Object.keys(source);
+
+    if (typeof Object.getOwnPropertySymbols === 'function') {
+      ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) {
+        return Object.getOwnPropertyDescriptor(source, sym).enumerable;
+      }));
+    }
+
+    ownKeys.forEach(function (key) {
+      _defineProperty(target, key, source[key]);
+    });
+  }
+
+  return target;
+}
+
 var plugin = {
   install: function install(Vue) {
     var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
@@ -34,7 +68,8 @@ var plugin = {
       env: options.env || 'production',
       version: options.version || '',
       disableReport: options.disableReport || false,
-      disableVueReport: options.disableVueReport || false
+      disableVueReport: options.disableVueReport || false,
+      config: {}
     }, options);
 
     if (_options.disableReport) {
@@ -42,10 +77,10 @@ var plugin = {
       console.log('Sentry has disabled');
     }
 
-    Raven.config(_options.dsn, {
+    Raven.config(_options.dsn, _objectSpread({
       environment: _options.env,
       release: _options.version
-    });
+    }, _options.config));
 
     if (!_options.disableVueReport) {
       Raven.addPlugin(RavenVue, Vue);
--- dist/vue-raven.js
+++ dist/vue-raven.js
@@ -1,5 +1,5 @@
 /*!
- * @ecg/vue-raven v2.1.0
+ * @ecg/vue-raven v2.2.0
  * (c) 2018-present savokiss <jaynaruto@qq.com>
  * Released under the MIT License.
  */
@@ -10,6 +10,21 @@
 }(this, (function (exports,Raven) { 'use strict';
 
   Raven = Raven && Raven.hasOwnProperty('default') ? Raven['default'] : Raven;
+
+  function _defineProperty(obj, key, value) {
+    if (key in obj) {
+      Object.defineProperty(obj, key, {
+        value: value,
+        enumerable: true,
+        configurable: true,
+        writable: true
+      });
+    } else {
+      obj[key] = value;
+    }
+
+    return obj;
+  }
 
   function _extends() {
     _extends = Object.assign || function (target) {
@@ -27,6 +42,25 @@
     };
 
     return _extends.apply(this, arguments);
+  }
+
+  function _objectSpread(target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i] != null ? arguments[i] : {};
+      var ownKeys = Object.keys(source);
+
+      if (typeof Object.getOwnPropertySymbols === 'function') {
+        ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) {
+          return Object.getOwnPropertyDescriptor(source, sym).enumerable;
+        }));
+      }
+
+      ownKeys.forEach(function (key) {
+        _defineProperty(target, key, source[key]);
+      });
+    }
+
+    return target;
   }
 
   /**
@@ -86,7 +120,8 @@
         env: options.env || 'production',
         version: options.version || '',
         disableReport: options.disableReport || false,
-        disableVueReport: options.disableVueReport || false
+        disableVueReport: options.disableVueReport || false,
+        config: {}
       }, options);
 
       if (_options.disableReport) {
@@ -94,10 +129,10 @@
         console.log('Sentry has disabled');
       }
 
-      Raven.config(_options.dsn, {
+      Raven.config(_options.dsn, _objectSpread({
         environment: _options.env,
         release: _options.version
-      });
+      }, _options.config));
 
       if (!_options.disableVueReport) {
         Raven.addPlugin(vue, Vue);
